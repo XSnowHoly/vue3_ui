2e6d39bb9b7bcb3e1442ebf73588368c27edfa3f
xsnowholy <495904127@qq.com>
2020-08-03 08:14:10 +0800

    feat: (DIY.vue): 新增自定义组件

---
+++ src/components/DIY.vue
@@ -0,0 +1,12 @@
+<!--
+ * @Description: 自定义组件
+ * @Author: George
+ * @Date: 2020-08-02 10:43:47
+ * @LastEditors: George
+ * @LastEditTime: 2020-08-02 10:44:42
+--> 
+<template>
+  <div>
+    DIY组件
+  </div>
+</template>
